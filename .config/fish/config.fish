0315d9897ed9911843938112884bdc80115c71fe
Hiro3328 <arturhbaron@gmail.com>
2024-11-24 10:31:12 -0300

    Fixes and some QOL improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,9 @@ set -x fish_greeting ""
 set -x FZF_DEFAULT_OPTS " --walker-skip .git,drive_z,z:
                           --preview 'tree -C {}'"
 
+fish_vi_key_bindings
+bind -M visual y fish_clipboard_copy
+bind p fish_clipboard_paste
 
 if status is-interactive   
   atuin init fish | source
@@ -21,3 +24,5 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+fish_add_path /home/hiro/.spicetify
